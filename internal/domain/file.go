3ffb1156bc2694552d33bf03f9be73703b5b8524
mac <mac@MacBook-Pro-mac-2.local>
2025-10-03 14:22:26 -0400

    feat(domain): Define core entities: File and UploadSpec

--- internal/domain/file.go
+++ internal/domain/file.go
@@ -1,1 +1,26 @@
 package domain
+
+import (
+	"time"
+)
+
+type File struct {
+	ID FileID
+	Name string
+	Size int64
+	ContentType string
+	Checksum Checksum
+	CreatedAt time.Time
+	OwnerID OwnerID
+	Tags []Tag
+	StorageRef string
+}
+
+type UploadSpec struct {
+	Name string
+	Size int64
+	ContentType string
+	IdempotencyKey string
+	OwnerID OwnerID
+	Tags []Tag
+}
